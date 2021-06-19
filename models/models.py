1be85279378f8183c2d4a58b872201eaeb8840e9
MouadZIANI <mouad.ziani1997@gmail.com>
2021-06-19 16:50:34 +0100

    feat: created models and database schemas

--- models/models.py
+++ models/models.py
@@ -1,10 +1,47 @@
 from db.configuration import Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 
-class User(Base):
-    __tablename__ = "users"
+class Category(Base):
+    __tablename__ = "categories"
     id = Column(Integer, primary_key=True,index=True)
     name = Column(String)
-    email = Column(String)
-    password = Column(String)
+
+
+class Website(Base):
+    __tablename__ = "websites"
+    id = Column(Integer, primary_key=True,index=True)
+    url = Column(String)
+    category_id = Column(Integer, ForeignKey("categories.id"))
+
+
+class Page(Base):
+    __tablename__ = "pages"
+    id = Column(Integer, primary_key=True,index=True)
+    url = Column(String)
+    website_id = Column(Integer, ForeignKey("websites.id"))
+
+
+class Scan(Base):
+    __tablename__ = "scans"
+    id = Column(Integer, primary_key=True,index=True)
+    startAt = Column(DateTime)
+    endAt = Column(DateTime)
+    page_id = Column(Integer, ForeignKey("pages.id"))
+
+
+class Rule(Base):
+    __tablename__ = "rules"
+    id = Column(Integer, primary_key=True,index=True)
+    name = Column(String)
+    type = Column(String)
+
+
+class ScanDetail(Base):
+    __tablename__ = "scan_details"
+    id = Column(Integer, primary_key=True,index=True)
+    score = Column(Integer)
+    scan_id = Column(Integer, ForeignKey("scans.id"))
+    rule_id = Column(Integer, ForeignKey("rules.id"))
+
+
